4a7f9005290247de021a3c609e89424b4b6d2610
Lupuna <terragod427@gmail.com>
2024-10-01 19:22:15 +0700

    setup cors headers

--- core/user_profile/managers.py
+++ core/user_profile/managers.py
@@ -2,26 +2,26 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
+    use_in_migrations = True
 
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            **extra_fields
-        )
+    def _create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError("The given username must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        return self._create_user(email, password, **extra_fields)
+
     def create_superuser(self, email, password=None, **extra_fields):
-        user = self.create_user(
-            email=email,
-            password=password,
-            **extra_fields
-        )
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
+        extra_fields.setdefault("is_staff", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        return self._create_user(email, password, **extra_fields)
 
