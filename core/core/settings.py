f52606d9cce5ef2446ba1bb9c1efb17578ec66fd
Lupuna <terragod427@gmail.com>
2024-10-07 22:00:55 +0700

    create nginx.conf, update settings.py, connect token verification

--- core/core/settings.py
+++ core/core/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-=s99!jrahn_)iiv+5n(-gv5l3*3hi^)m37@60@60ib^um3!d*i
 load_dotenv()
 DEBUG = os.environ.get('IS_DEBUG')
 
-ALLOWED_HOSTS = ["localhost", "92.63.76.98"]
+ALLOWED_HOSTS = ["localhost", "92.63.76.98", '127.0.0.1']
 
 
 # Application definition
@@ -214,3 +214,5 @@ STORAGE_ACCESS_KEY = os.getenv('ACCESS_STORAGE_KEY')
 STORAGE_SECRET_KEY = os.getenv('SECRET_STORAGE_KEY')
 BUCKET_NAME = 'bucket-for-user-avatar'
 STORAGE_URL = f'https://s3.storage.selcloud.ru/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
