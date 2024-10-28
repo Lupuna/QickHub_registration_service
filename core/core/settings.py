45d9c360646268042e24f8360ae6dc2f5c51749a
Lupuna <terragod427@gmail.com>
2024-10-28 16:41:33 +0700

    debug documentation

--- core/core/settings.py
+++ core/core/settings.py
@@ -149,9 +149,9 @@ SIMPLE_JWT = {
 
     'BLACKLIST_AFTER_ROTATION': True,
 
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
 
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
 
 }
 
